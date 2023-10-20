7947edca10007fc6b92bee6718d070bbd8b08ab9
Jcidcab84 <juancid62@gmail.com>
2023-10-20 20:25:02 -0300

    listo

--- assets/js/arriendo.js
+++ assets/js/arriendo.js
@@ -1,23 +1,59 @@
-const arriendos = [
-    {
+const arriendos = [ {
+    nombre: 'Casa en La Florida Alto',
+    src: './assets/img/img-main.jpeg',
+    descripcion: 'Amplia casa, cercana a metro',
+    ubicacion: 'Vicuña Mackenna',
+    habitaciones: 3,
+    banos: 2,
+    costo: 2000,
+    smoke: true,
+    pets: true,
+  },  
+  {
     nombre: 'Casa en Puente Alto',
     src: './assets/img/img-main.jpeg',
     descripcion: 'Amplia casa, cercana a colegios',
     ubicacion: 'Av. Tobalaba',
     habitaciones: 3,
     banos: 2,
-    costo: 2000,
+    costo: 5000,
+    smoke: false,
+    pets: false,
+  }, 
+  {
+    nombre: 'Departamento en Majadilla',
+    src: './assets/img/img-main.jpeg',
+    descripcion: 'Con vista al rio',
+    ubicacion: 'Ruta J60',
+    habitaciones: 4,
+    banos: 2,
+    costo: 4500,
+    smoke: false,
+    pets: true,
+  }, 
+  {
+    nombre: 'Casa en Maipu',
+    src: './assets/img/img-main.jpeg',
+    descripcion: 'Amplia casa, cercana a colegios',
+    ubicacion: 'Av. Tobalaba',
+    habitaciones: 3,
+    banos: 1,
+    costo: 3000,
     smoke: true,
-    pets: true
-    },
-        
+    pets: false,
+  }, 
+]
 
-    ]
+    
     const contenedorArriendo = document.querySelector("#arriendo");
-    let html = ''
-    for ( const arriendo of arriendos){
 
-        html += `<div class= "card">
+
+
+    for ( let arriendo of arriendos){
+        
+        let autorizacionFumar = sePuedeFumar(arriendo.smoke);
+        let autorizacionMacota = sePuedeTenerMAscotas(arriendo.pets);
+        let html = `<div class= "card">
         <img src="${arriendo.src}" alt="" class="card img">
         <h2>${arriendo.nombre}</h2>
         <p>${arriendo.descripcion}</p>
@@ -25,45 +61,45 @@ const arriendos = [
         <p>Habitaciones : ${arriendo.habitaciones}</p>
         <p>Baños : ${arriendo.banos}</p>
         <p>Costo : $${arriendo.costo}</p>
-        <p> ${smoke(arriendo.smoke)} </p>
-        <p> ${mascota(arriendo.mascota)} </p>
-       
-          
+
+        ${autorizacionFumar}
+        ${autorizacionMacota}
         
-      </div>
-        `
+         
+      </div>`
+      contenedorArriendo.innerHTML += html
     } 
 
-    contenedorArriendo.innerHTML = html
 
-     // Funcion para fumar
-     function smoke(fuego){
-      let cigarro = ""
-    
-      if(fuego == true){
-        cigarro = " Permitido fumar";
-        // cigarro.style.color = 'green';
-       
-      }else{
-        cigarro ="No se permite fumar";
+
+
+    function sePuedeFumar(smoke){
+        if(smoke==true){
+          let permisoParaFumar = `
+          <p class="fuma"> Se permite fumar</p>
+          `
+          return permisoParaFumar;
+        }else{
+          let noPermisoParaFumar = `
+          <p class="nofuma"> No se permite fumar</p>
+            `
+            return noPermisoParaFumar;
+        }
         
-      }
-      return cigarro
+      
     }
 
-
-
-  //  Funcion para mascotas
-     function mascota(perro){
-      let animal = ""
-    
-      if(perro == true){
-        animal = "Puedes tener mascota";
-       
-      }else{
-        animal ="No puedes tener mascota";
-        
-      }
-      return animal
+    function sePuedeTenerMAscotas(pets){
+      if(pets==true){
+        let permisoParaMascotas =  `
+        <p class="mascota"> Se permiten mascotas</p>
+        `
+        return permisoParaMascotas;
+    } else {
+        let noPermisoParaMascotas= `
+        <p class="nomascota"> No se permiten mascotas</p>
+        `
+        return noPermisoParaMascotas;
     }
-
+  
+  }
--- assets/js/venta.js
+++ assets/js/venta.js
@@ -1,69 +1,108 @@
 const ventas = [
-    {
+  {
     nombre: 'Casa en La Florida',
     src: './assets/img/img-main.jpeg',
     descripcion: 'Amplia casa, cercana a esta<cion del metro',
     ubicacion: 'Av. Vicuña Mackenna',
     habitaciones: 4,
     banos: 3,
-    costo: 5200000,
+    costo: 3500000,
     smoke: true,
     pets: true
     },
         
+    {
+    nombre: 'Departamento en La Florida',
+    src: './assets/img/img-main.jpeg',
+    descripcion: 'Amplia casa, cercana a esta<cion del metro',
+    ubicacion: 'Av. Vicuña Mackenna',
+    habitaciones: 2,
+    banos: 2,
+    costo: 5200000,
+    smoke: false,  
+    pets: true,
+    },
+
+    {
+    nombre: 'Casa en La Granja',
+    src: './assets/img/img-main.jpeg',
+    descripcion: 'cercana a colegios',
+    ubicacion: 'Villa los pensamientos',
+    habitaciones: 4,
+    banos: 3,
+    costo: 4500000,
+    smoke: true,
+    pets: false
+    },
+
+    {
+    nombre: 'Casa en Providencia',
+    src: './assets/img/img-main.jpeg',
+    descripcion: 'Barrio comercial',
+    ubicacion: 'Carlos Antunez',
+    habitaciones: 4,
+    banos: 3,
+    costo: 10000000,
+    smoke: false,
+    pets: false
+     },
 
     ]
     const contenedorVenta = document.querySelector("#venta");
-    let html = ''
-    for ( const venta of ventas){
 
-        html += `<div class= "card">
+
+  
+    for ( let venta of ventas){
+
+        let autorizacionFumar = sePuedeFumar(venta.smoke);
+        let autorizacionMacota = sePuedeTenerMAscotas(venta.pets);
+        let html = `<div class= "card">
         <img src="${venta.src}" alt="" class="card img">
         <h2>${venta.nombre}</h2>
         <p>${venta.descripcion}</p>
         <p>Ubicacion : ${venta.ubicacion}</p>
         <p>Habitaciones : ${venta.habitaciones}</p>
-        <p>Baños :<i class="fas fa-bed"></i>     ${venta.banos}</p>
+        <p>Baños : ${venta.banos}</p>
         <p>Costo : $${venta.costo}</p>
-        <p> ${smoke(venta.smoke)} </p>
-        <p> ${mascota(venta.mascota)} </p>
-       
-          
+
+        ${autorizacionFumar}
+        ${autorizacionMacota}
         
-      </div>
-        `
+         
+      </div>`
+      contenedorVenta.innerHTML += html
     } 
 
-    contenedorVenta.innerHTML = html
 
-     // Funcion para fumar
-     function smoke(fuego){
-      let cigarro = ""
-    
-      if(fuego == true){
-        cigarro = " Permitido fumar";
-        // cigarro.style.color = 'green';
-       
+   
+
+    function sePuedeFumar(smoke){
+      if(smoke==true){
+        let permisoParaFumar = `
+        <p class="fuma"> Se permite fumar</p>
+        `
+        return permisoParaFumar;
       }else{
-        cigarro ="No se permite fumar";
-        
+        let noPermisoParaFumar = `
+        <p class="nofuma"> No se permite fumar</p>
+          `
+          return noPermisoParaFumar;
       }
-      return cigarro
-    }
-
-
-
-  //  Funcion para mascotas
-     function mascota(perro){
-      let animal = ""
+      
     
-      if(perro == true){
-        animal = "Puedes tener mascota";
-       
-      }else{
-        animal ="No puedes tener mascota";
-        
-      }
-      return animal
-    }
+  }
 
+  function sePuedeTenerMAscotas(pets){
+    if(pets==true){
+      let permisoParaMascotas =  `
+      <p class="mascota"> Se permiten mascotas</p>
+      `
+      return permisoParaMascotas;
+  } else {
+      let noPermisoParaMascotas= `
+      <p class="nomascota"> No se permiten mascotas</p>
+      `
+      return noPermisoParaMascotas;
+  }
+
+}
